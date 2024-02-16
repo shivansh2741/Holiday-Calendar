62b7d7edc972788c2bbbfa4d0404247f5701920a
shivansh2741 <shivanshd10@gmail.com>
2024-02-17 00:34:00 +0530

    calendar commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
-import { generateDate, months } from "./util/calender";
-import cn from "./util/cn";
-import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { FaMapMarkerAlt } from 'react-icons/fa';
-import { fetchHolidaysByCountryAndMonth } from './services/fetchHolidays'
-import countries from './util/countries'
-
+import { fetchAllCountries } from "./services/fetchCountries";
+import Calendar from "./components/Calendar"
 
 interface Country {
-	name: string;
-	code: string;
+    countryCode: string;
+    name: string;
 }
 
 interface Holiday {
@@ -28,33 +24,15 @@ interface Holiday {
 	week_day: string;
 }
 
-export default function Calendar() {
-	const days = ["S", "M", "T", "W", "T", "F", "S"];
-	const currentDate = dayjs();
-	const [today, setToday] = useState(currentDate);
-	const [selectDate, setSelectDate] = useState(currentDate);
+export default function App() {
+	
+    const currentDate = dayjs();
+	
 	const [selectedOption, setSelectedOption] = useState<'month' | 'year'>('month');
 	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const [selectedCountry, setSelectedCountry] = useState<Country>({ name: 'Ukraine', code: 'UA' });
+	const [selectedCountry, setSelectedCountry] = useState<Country>({ countryCode: 'Ukraine', name: 'UA' });
 	const [holidays, setHolidays] = useState<Holiday[]>([]);
-
-	useEffect(() => {
-		const fetchHolidays = async () => {
-			const currentYear: string = (today.year()).toString();
-
-			const data: Holiday[] = await fetchHolidaysByCountryAndMonth(selectedCountry.code, currentYear);
-
-			if (data.length !== 0) {
-				setHolidays(data);
-				console.log(data);
-			}
-		}
-
-
-		fetchHolidays();
-	}, [selectedCountry, today.year()])
-
-
+	const [countries, setCountries] = useState<Country[]>([]);
 
 	const toggleMenu = () => {
 		setIsOpen((prev) => !prev);
@@ -73,49 +51,21 @@ export default function Calendar() {
 		toggleMenu();
 	};
 
-	const getFormattedDate = (date: number, month: number, year: number): string => {
-		const paddedMonth = (month + 1).toString().padStart(2, '0');
-		const paddedDate = date.toString().padStart(2, '0');
-		const formattedDate = `${year}-${paddedMonth}-${paddedDate}`;
-		return formattedDate;
-	};
 
-	const condition = (date: number, month: number, year: number) => {
-		const formattedDate = getFormattedDate(date, month, year);
+	
 
-		return holidays.some(holiday =>
-			holiday.date === formattedDate
-		);
-	};
+	useEffect( () => {
 
+		fetchAllCountries().then(
+			(data) => {
+				setCountries(data);
+				console.log(data);
+			}
+		)
+		
+		
 
-	const [popupVisible, setPopupVisible] = useState(false);
-	const [hoveredDate, setHoveredDate] = useState<number | null>(null);
-	const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
-
-
-	const handleMouseEnter = (index: number) => {
-		const id = setTimeout(() => {
-			setPopupVisible(true);
-			setHoveredDate(index);
-		}, 0);
-		setTimeoutId(id);
-	};
-
-	const handleMouseLeave = () => {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
-		setPopupVisible(false);
-		setHoveredDate(null);
-	};
-
-	const fetchHolidayDetails = (date: number, month: number, year: number) => {
-		const formattedDate = getFormattedDate(date, month, year);
-
-		const holiday = holidays.find(holiday => holiday.date === formattedDate);
-		return holiday ? holiday.name : undefined;
-	}
+	}, [])
 
 	return (
 		<div className="flex justify-center items-center h-screen">
@@ -177,91 +127,7 @@ export default function Calendar() {
 					)}
 				{(selectedOption === 'month') &&
 					<div className="max-w-[100rem] max-h-[50rem]">
-						<div className="flex justify-between items-center">
-							<h1 className="select-none font-semibold">
-								{months[today.month()]}, {today.year()}
-							</h1>
-							<div className="flex gap-10 items-center ">
-								<GrFormPrevious
-									className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
-									onClick={() => {
-										setToday(today.month(today.month() - 1));
-									}}
-								/>
-								<h1
-									className=" cursor-pointer hover:scale-105 transition-all"
-									onClick={() => {
-										setToday(currentDate);
-									}}
-								>
-									Today
-								</h1>
-								<GrFormNext
-									className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
-									onClick={() => {
-										setToday(today.month(today.month() + 1));
-									}}
-								/>
-							</div>
-						</div>
-						<div className="grid grid-cols-7 ">
-							{days.map((day, index) => {
-								return (
-									<h1
-										key={index}
-										className="text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none"
-									>
-										{day}
-									</h1>
-								);
-							})}
-						</div>
-
-						<div className=" grid grid-cols-7">
-							{generateDate(today.month(), today.year()).map(
-								({ date, currentMonth, today }, index) => {
-									return (
-										<div
-											key={index}
-											className="p-2 text-center h-14 grid place-content-center text-sm border"
-											onMouseEnter={() => handleMouseEnter(index)}
-											onMouseLeave={handleMouseLeave}
-											onClick={() => console.log(date)}
-										>
-											<h1
-												className={cn(
-													currentMonth ? "" : "text-gray-400",
-													today
-														? "bg-red-600 text-white"
-														: condition(date.date(), date.month(), date.year())
-															? "rounded-full bg-blue-500 text-white h-10 w-10 grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
-															: "",
-													selectDate.toDate().toDateString() ===
-														date.toDate().toDateString()
-														? "bg-black text-white"
-														: "",
-													"h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
-												)}
-												onClick={() => {
-													setSelectDate(date);
-												}}
-											>
-												{date.date()}
-											</h1>
-											{condition(date.date(), date.month(), date.year()) && popupVisible && hoveredDate === index && (
-												<div
-													className="absolute mt-12 ml-4 bg-blue-500 text-white border border-gray-300 shadow-lg rounded-lg p-4"
-													style={{ zIndex: 999 }}
-												>
-													<p>{fetchHolidayDetails(date.date(), date.month(), date.year())}</p>
-												</div>
-
-											)}
-										</div>
-									);
-								}
-							)}
-						</div>
+						<Calendar selectedCountry={selectedCountry}/>
 					</div>}
 				{(selectedOption === 'year') &&
 					<div> this is where year goes </div>
