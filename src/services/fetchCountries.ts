c61d2772cabaeda238b26c37b1162b3fa149c3ef
shivansh2741 <shivanshd10@gmail.com>
2024-02-17 00:34:59 +0530

    Added service to fetch countries

---
+++ src/services/fetchCountries.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_KEY: string = 'e2d94ccc7c5b4b558ac169d10eeb5751';
+const url = 'https://date.nager.at/api/v3';
+interface Country {
+    countryCode: string;
+    name: string;
+}
+
+export  async function fetchAllCountries(): Promise<Country[]> {
+    return axios.get<Country[]>(`${url}/AvailableCountries`)
+    .then(response => {
+        const countries = response.data
+        console.log(countries);
+        return countries;
+    })
+    .catch(error => {
+        console.log(error);
+        return [];
+    });
+}
+
+
