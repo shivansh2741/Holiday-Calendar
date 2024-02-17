4f684f4f3e4d4265002c3d1a06489b0884ce32b8
s-hivansh <shivansh.dwivedi@maidaana.app>
2024-02-17 13:05:28 +0530

    added i button

--- src/App.tsx
+++ src/App.tsx
@@ -67,31 +67,12 @@ export default function App() {
 
 
 	}, [])
+
+	const [showMessage, setShowMessage] = React.useState(false);
 	const months = Array.from({ length: 12 }, (_, index) => index + 1);
 	return (
 		<div className="flex justify-center items-center h-screen">
-			<div className="flex flex-col items-center">
-
-				<div className="flex items-center mb-2">
-					<div className="w-4 h-4 rounded-full bg-red-500 mr-4"></div>
-					<p className="text-base">Today's Date</p>
-				</div>
-
-
-				<div className="flex items-center mb-2">
-					<div className="w-4 h-4 rounded-full bg-blue-500 mr-2"></div>
-					<p className="text-base">Public Holiday</p>
-				</div>
-
-
-				<div className="flex items-center">
-					<div className="w-4 h-4 rounded-full bg-black mr-2"></div>
-					<p className="text-base">Selected Date</p>
-				</div>
-			</div>
-
-
-			<div className="flex gap-10 sm:divide-x justify-center h-screen items-center sm:flex-col flex-col m-10">
+			<div className="flex gap-10 justify-center h-screen items-center sm:flex-col flex-col m-10">
 				<div>
 					<button
 						className={`mr-2 px-4 py-2 rounded ${selectedOption === 'month' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
@@ -108,10 +89,43 @@ export default function App() {
 						Year
 					</button>
 				</div>
-				<button onClick={toggleMenu} className="px-4 py-2 bg-gray-200 border border-gray-300 rounded flex items-center">
-					<FaMapMarkerAlt className="mr-2 text-gray-500" />
-					{selectedCountry.name}
-				</button>
+				<div className="flex items-center justify-between">
+					<div className="relative inline-block mr-4">
+						<button
+							className="bg-gray-500 text-white rounded-full w-8 h-8 flex items-center justify-center transition-colors duration-300 hover:bg-blue-700"
+							onMouseEnter={() => setShowMessage(true)}
+							onMouseLeave={() => setShowMessage(false)}
+						>
+							<span className="text-xs">i</span>
+						</button>
+						{showMessage && (
+							<div className="absolute top-full left-1/2 transform -translate-x-1/2 bg-gray-100 text-gray-700 p-4 rounded-md shadow-md w-40 bg-opacity-90">
+								<div className="flex flex-col items-center">
+									<div className="flex items-center mb-2">
+										<div className="w-4 h-4 rounded-full bg-red-500 mr-2"></div>
+										<p className="text-sm">Today's Date</p>
+									</div>
+									<div className="flex items-center mb-2">
+										<div className="w-4 h-4 rounded-full bg-blue-500 mr-2"></div>
+										<p className="text-sm">Public Holiday</p>
+									</div>
+									<div className="flex items-center">
+										<div className="w-4 h-4 rounded-full bg-black mr-2"></div>
+										<p className="text-sm">Selected Date</p>
+									</div>
+								</div>
+							</div>
+						)}
+					</div>
+
+
+					<button onClick={toggleMenu} className="px-4 py-2 bg-gray-200 border border-gray-300 rounded flex items-center">
+						<FaMapMarkerAlt className="mr-2 text-gray-500" />
+						{selectedCountry.name}
+					</button>
+				</div>
+
+
 				{isOpen &&
 					(
 						<ul className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-md bg-white border border-gray-300 rounded shadow-md">
@@ -135,14 +149,14 @@ export default function App() {
 						{months.map((month, index) => {
 
 							const todayForMonth = dayjs().startOf('year').add(index, 'month');
-		
+
 							return (
 								<Calendar
 									key={month}
 									selectedCountry={selectedCountry}
 									selectedOption={selectedOption}
 									today={todayForMonth}
-									setToday={() => { }} 
+									setToday={() => { }}
 								/>
 							);
 						})}
